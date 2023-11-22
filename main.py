401b861904bca372fdb115c91d030fefbb5a6e42
Vinícius <petiniprojetos96@gmail.com>
2023-11-21 20:17:30 -0400

    adicionado funções

--- main.py
+++ main.py
@@ -12,22 +12,37 @@ conn = psycopg2.connect(
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM users")
 
-users = cursor.fetchall()
-for user in users:
-    if type(user[4]) == NoneType:
-        img = qrcode.make("Olá! " + user[1]+ " faça uma recarga.")
-        type(img)
-        img.save(user[1] + ".png")
-    else:
-        img = qrcode.make("Olá! " + user[1]+ ", você tem " + str(user[4]))
-        type(img)
-        img.save(user[1] + ".png")
-    
-    
-    print(users)
+def generateQr(userId):
+  img = qrcode.make(userId)
+  type(img)
+  img.save("qr-user-" + str(userId) + ".png")
+  print("Hello from a function")
 
+def getCredits(userId):
+    cursor.execute("SELECT total_value FROM users WHERE id = " + str(userId))
+    userTotalValue = cursor.fetchone()
+    print(str(userTotalValue))
+
+def getAllUsers():
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    for user in users:
+        if type(user[4]) == NoneType:
+            img = qrcode.make("Olá! " + user[1]+ " faça uma recarga.")
+            type(img)
+            img.save(user[1] + ".png")
+        else:
+            img = qrcode.make("Olá! " + user[1]+ ", você tem " + str(user[4]))
+            type(img)
+            img.save(user[1] + ".png")
+        print(user[1])
+
+def setCredits(userId, value):
+    cursor.execute("UPDATE users SET total_value = "+str(value)+" WHERE id = " + str(userId))
+
+setCredits(3, 10.0)
+getCredits(3)
 cursor.close()
 conn.close()
 
--- qr.py
+++ qr.py
@@ -1,11 +1,22 @@
-import qrcode
+# import qrcode
 
+# def generateQr(userId):
+#   img = qrcode.make(userId)
+#   type(img)
+#   img.save("qr-user-" + str(userId) + ".png")
+#   print("Hello from a function")
 
-# function generateQr(userId) {
-#     img = qrcode.make(text)
-#     type(img)  # qrcode.image.pil.PilImage
-#     img.save("some_file.png")
-# }
+# def verifyCredit(userId):
+#   img = qrcode.make(userId)
+#   type(img)
+#   img.save("qr-user-" + str(userId) + ".png")
+#   print("Hello from a function")
+
+# generateQr(1)
+# generateQr(2)
+# generateQr(3)
+# generateQr(4)
+
 
 # function generateQr(userId) {
 #     img = qrcode.make(text)
